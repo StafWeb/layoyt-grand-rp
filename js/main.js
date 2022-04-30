7f07d4f4dab9f77bd8f2d727cb91bc59e491dc8b
StafWeb <staf.target@yandex.ru>
2022-04-30 23:01:08 +0300

    burger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+const burger = document?.querySelector('[data-burger]');
+const nav = document?.querySelector('[data-nav]');
+const navItems = nav?.querySelectorAll('.header__nav_link');
+const body = document.body;
+const header = document?.querySelector('.header');
+const headerHeight = header.offsetHeight;
+document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
+
+burger?.addEventListener('click', () => {
+//   body.classList.toggle('stop-scroll');
+  burger?.classList.toggle('burger--active');
+  nav?.classList.toggle('nav--visible');
+});
+
+navItems.forEach(el => {
+  el.addEventListener('click', () => {
+    // body.classList.remove('stop-scroll');
+  burger?.classList.remove('burger--active');
+  nav?.classList.remove('nav--visible');
+  });
+});
+
 const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
@@ -14,3 +36,4 @@ const modal = new GraphModal({
 
 	}
 });
+
