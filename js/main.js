96f61bf6851e58bde889268a789d0a58849c036f
StafWeb <staf.target@yandex.ru>
2022-04-30 19:06:26 +0300

    modal

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
 const swiper = new Swiper('.swiper', {
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
     },
-  });
+});
+
+const modal = new GraphModal({
+	isOpen: (modal) => {
+        
+	},
+	isClose: () => {
+
+	}
+});
