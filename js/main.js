ac05ddf7781fdf0b3f885d86e15cdda63a9f4f16
StafWeb <staf.target@yandex.ru>
2022-05-02 00:06:42 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,22 +1,25 @@
 const burger = document?.querySelector('[data-burger]');
 const nav = document?.querySelector('[data-nav]');
 const navItems = nav?.querySelectorAll('.header__nav_link');
+const overlayNav = document?.querySelector(".overlay-nav");
 const body = document.body;
 const header = document?.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 
 burger?.addEventListener('click', () => {
-//   body.classList.toggle('stop-scroll');
+  body.classList.toggle('stop-scroll');
   burger?.classList.toggle('burger--active');
   nav?.classList.toggle('nav--visible');
+  overlayNav.classList.toggle('show');
 });
 
 navItems.forEach(el => {
   el.addEventListener('click', () => {
-    // body.classList.remove('stop-scroll');
-  burger?.classList.remove('burger--active');
-  nav?.classList.remove('nav--visible');
+    body.classList.remove('stop-scroll');
+    burger?.classList.remove('burger--active');
+    nav?.classList.remove('nav--visible');
+    overlayNav.classList.remove('show');
   });
 });
 
@@ -27,13 +30,19 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
 });
+const modal = new GraphModal();
+const vidos = document.querySelectorAll('.video');
+const modals = document.querySelectorAll('.graph-modal__container');
 
-const modal = new GraphModal({
-	isOpen: (modal) => {
-        
-	},
-	isClose: () => {
-
-	}
+// btnClose.addEventListener('click', () => {
+//     const videoLink = vidos.src;
+//     vidos.src = '';
+//     vidos.src = videoLink;
+//   });
+modals.forEach(el => {
+    el.addEventListener('click', () => {
+        const videoLink = vidos.src;
+        vidos.src = '';
+        vidos.src = videoLink;
+    });
 });
-
