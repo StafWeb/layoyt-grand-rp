080eefbbae2a15e89b1d8ad6e688ded304c443c6
StafWeb <staf.target@yandex.ru>
2022-05-04 18:37:04 +0300

    lang list

--- js/main.js
+++ js/main.js
@@ -1,28 +1,36 @@
 const burger = document?.querySelector('[data-burger]');
 const nav = document?.querySelector('[data-nav]');
 const navItems = nav?.querySelectorAll('.header__nav_link');
-const overlayNav = document?.querySelector(".overlay-nav");
+const overlayNav = document?.querySelector('.overlay-nav');
 const body = document.body;
 const header = document?.querySelector('.header');
 const headerHeight = header.offsetHeight;
 document.querySelector(':root').style.setProperty('--header-height', `${headerHeight}px`);
 
 burger?.addEventListener('click', () => {
-  body.classList.toggle('stop-scroll');
-  burger?.classList.toggle('burger--active');
-  nav?.classList.toggle('nav--visible');
-  overlayNav.classList.toggle('show');
+    body.classList.toggle('stop-scroll');
+    burger?.classList.toggle('burger--active');
+    nav?.classList.toggle('nav--visible');
+    overlayNav.classList.toggle('show');
 });
 
 navItems.forEach(el => {
-  el.addEventListener('click', () => {
-    body.classList.remove('stop-scroll');
-    burger?.classList.remove('burger--active');
-    nav?.classList.remove('nav--visible');
-    overlayNav.classList.remove('show');
-  });
+    el.addEventListener('click', () => {
+        body.classList.remove('stop-scroll');
+        burger?.classList.remove('burger--active');
+        nav?.classList.remove('nav--visible');
+        overlayNav.classList.remove('show');
+    });
 });
 
+window.addEventListener('click', function (event) {
+    if (event.target.dataset.lang === 'btn-lang') {
+        const langWrapper = event.target.closest('.header__lang')
+        const langList = langWrapper.querySelector('.lang_list')
+        langList.classList.toggle('show');
+    }
+
+});
 const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
@@ -34,16 +42,17 @@ const swiper = new Swiper('.swiper', {
 const videos = document.querySelectorAll('iframe');
 
 const modal = new GraphModal({
-	isOpen: (modal) => {
+    isOpen: (modal) => {
 
-	},
-	isClose: () => {
-        for (let vid of videos){
+    },
+    isClose: () => {
+        for (let vid of videos) {
             const videoLink = vid.src;
-        vid.src = '';
-        vid.src = videoLink;
-        console.log(close);
+            vid.src = '';
+            vid.src = videoLink;
+            console.log(close);
         };
-        
-	}
+
+    }
 });
+
